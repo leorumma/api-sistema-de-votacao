053e6dcdc48a7504cb56cd87441e8c575b585110
Leonardo Ruma <leonardo.ruma@gmail.com>
2022-07-30 22:28:24 -0300

    adicionando dependencias

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8ff5fd3a7dd644bb8d607e7c54a92f238467cccd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8ff5fd3a7dd644bb8d607e7c54a92f238467cccd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
